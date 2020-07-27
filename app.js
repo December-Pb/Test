8649055015eb47d24e0a2f2aaca03858264bfc1d
Jackie Sun <jackie@MacBook-Pro.local>
2020-07-27 16:19:29 -0400

    debug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express"),
     app = express();
 
 app.get("/", function(req, res) {
-    res.redirect("blogs");
+    res.send("Hi there!");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function() {
